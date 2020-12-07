c960e635af7988eafb3d60e76da5030155399ce8
清川　裕太郎 <yuu7353@gmail.com>
2020-12-07 19:24:58 +0900

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/**
+ *@author
+ *@function Header
+ **/
+
+const Header = props => {
+  return (
+    <header>
+      <nav>
+        <a href="#"></a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
