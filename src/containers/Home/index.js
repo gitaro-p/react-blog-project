8d1fd71308d50519e3ece5d65b9bad6e42be779b
清川　裕太郎 <yuu7353@gmail.com>
2020-12-07 20:55:16 +0900

    header

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './style.css';
+import Header from '../../components/Header';
 
 const Home = props => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
