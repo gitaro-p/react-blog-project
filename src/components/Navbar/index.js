667a74744f96bdd03d8d61764a630a180ed2ed64
清川　裕太郎 <yuu7353@gmail.com>
2020-12-08 22:39:27 +0900

    navigation & hero

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.css';
 
 /**
  * @author
@@ -6,7 +7,24 @@ import React from 'react';
  **/
 
 const Navbar = props => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <ul className="navbarMenu">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About Us</a>
+        </li>
+        <li>
+          <a href="#">Posts</a>
+        </li>
+        <li>
+          <a href="#">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
